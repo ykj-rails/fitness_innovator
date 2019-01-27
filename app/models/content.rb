c2ce5b5ebab973c123221eff8818135851b12fd6
Yuki M <yukiji.0307@gmail.com>
2019-01-28 00:30:04 +0900

    model_update

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,4 @@
 class Content < ApplicationRecord
+  belongs_to :user
+  validates :title, :before_body_weight, :after_body_weight, :period, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :contents
+
+  validates :nickname, presence: true, length: { maximum: 10 }
 end
