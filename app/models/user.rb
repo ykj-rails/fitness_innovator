ca8ea9cc8999802866ab6bb38e3498b65063a5b1
Yuki M <yukiji.0307@gmail.com>
2019-02-01 21:21:16 +0900

    user_devise_view_complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable
+         :recoverable, :rememberable, :validatable,
+         :timeoutable, :confirmable, :trackable
 
   has_many :contents
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # default url
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "rails5test.ykj@gmail.com",
+    :password => ENV['GMAIL_PASS_TEST'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
