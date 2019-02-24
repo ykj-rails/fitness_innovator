f2df2931ec0d90138ed9eade65c4f185d71906f9
Yuki M <yukiji.0307@gmail.com>
2019-02-24 16:01:49 +0900

    deviseビューファイル修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys:[:nickname, :profile])
   end
+
 end
