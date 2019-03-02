c1d42a9b853192347f7f37331ccba396099145ad
Yuki M <yukiji.0307@gmail.com>
2019-03-02 10:12:24 +0900

    workoutヘルパーメソッド定義

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,2 +1,12 @@
 module WorkoutsHelper
+
+  def day_of_the_week(week_id)
+    return week_set[week_id]
+  end
+
+  private
+  def week_set
+    week = ["オフの日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
+  end
+
 end
