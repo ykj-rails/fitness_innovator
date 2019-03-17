08c9e392811a9d23ca64beeae2c1b30c99b0959c
Yuki M <yukiji.0307@gmail.com>
2019-03-17 17:43:23 +0900

    ユーザーマイページ下部コンテンツ完成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:destroy]
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
@@ -8,15 +7,12 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
     @comment.destroy if @comment.user_id == current_user.id
     redirect_back(fallback_location: root_path)
   end
 
   private
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
     def comment_params
       params.require(:comment).permit(:text).merge(content_id: params[:content_id], user_id: current_user.id)
     end
