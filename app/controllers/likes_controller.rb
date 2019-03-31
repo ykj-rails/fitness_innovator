fb5e1c6a23167b6f739c3e810cd1d7c4525c5f9e
Yuki M <yukiji.0307@gmail.com>
2019-03-31 20:24:07 +0900

    いいねアニメーション追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,24 @@
 class LikesController < ApplicationController
 
-
   def create
     @content = Content.find(params[:content_id])
-    @content.like_it(current_user)
+    unless @content.like_it?(current_user)
+      @content.like_it(current_user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
 
   def destroy
     @content = Like.find(params[:id]).content
-    @content.unlike_it(current_user)
+    if @content.like_it?(current_user)
+      @content.unlike_it(current_user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
-
 end
