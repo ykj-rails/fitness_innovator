adb0187ed061c1ddf95051fce69efd882c99457e
Yuki M <yukiji.0307@gmail.com>
2019-04-07 11:06:19 +0900

    アラート部分テンプレート化

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,9 +1,18 @@
+// アラートアニメーション
 $(function() {
-  var notice = $('#alert-notice');
-
+  var notice = $('#alert-notice'),
+      like = $('#like-alert');
+  // notice(通知系)アラート
   setTimeout(function(){
-    notice.slideUp(800);
+    notice.slideUp(600);
   },4500);
+  notice.slideDown(200);
+  // 非ログイン時のいいねアラート
+  $('.like_button').on('click', function() {
+    setTimeout(function(){
+      like.slideUp(400);
+    },3500);
+    like.slideDown(200);
+  });
 
-  notice.slideDown(300);
 });
