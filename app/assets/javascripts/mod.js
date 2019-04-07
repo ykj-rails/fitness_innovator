ba9e0324501f8de08629d053ecc78c4958e193bd
Yuki M <yukiji.0307@gmail.com>
2019-04-07 19:55:08 +0900

    ユーザーマイページタブ切り替えJs実装

--- app/assets/javascripts/mod.js
+++ app/assets/javascripts/mod.js
@@ -15,3 +15,27 @@ $(function() {
     $header.remove();
   });
 });
+
+// ボタンタブ切り替え(user/show下部コンテンツ)
+$(function() {
+  var active = 'active-top',
+      userImageButton = $('#user_image-button'),
+      userImageContent = $('.user_image'),
+      bookMarkButton = $('#user_bookmark_image-button'),
+      bookMarkContent = $('.user_bookmark_image');
+
+  $('.nav-button').on('click', function(){
+    buttonId = $(this).attr('id');
+    if ( buttonId ===  'user_image-button' ) {
+      userImageButton.addClass(active);
+      userImageContent.removeClass('none');
+      bookMarkButton.removeClass(active);
+      bookMarkContent.addClass('none');
+    } else {
+      bookMarkButton.addClass(active);
+      bookMarkContent.removeClass('none');
+      userImageButton.removeClass(active);
+      userImageContent.addClass('none');
+    }
+  });
+});
