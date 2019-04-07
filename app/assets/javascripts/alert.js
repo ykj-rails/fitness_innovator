d2f12a16106db21ee21f216e42ccc8867d695bd1
Yuki M <yukiji.0307@gmail.com>
2019-04-07 21:47:41 +0900

    ブックマーク用アラート設定

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,18 +1,17 @@
 // アラートアニメーション
 $(function() {
-  var notice = $('#alert-notice'),
-      like = $('#like-alert');
+  var notice = $('#alert-notice');
   // notice(通知系)アラート
   setTimeout(function(){
     notice.slideUp(600);
   },4500);
   notice.slideDown(200);
-  // 非ログイン時のいいねアラート
-  $('.like_button').on('click', function() {
+  // 非ログイン時のアラート
+  $('.like_button,.bookmark_button').on('click', function() {
+    var error = $(this).attr('class');
     setTimeout(function(){
-      like.slideUp(400);
+      $(`#${error}-alert`).slideUp(400);
     },3500);
-    like.slideDown(200);
+    $(`#${error}-alert`).slideDown(200);
   });
-
 });
