1d42ac6cac84fe4ba08dfb33339dd4f1ce6c3889
Yuki M <yukiji.0307@gmail.com>
2019-04-08 23:11:22 +0900

    パスワード表示切り替え機能実装

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -4,9 +4,9 @@ $(function() {
       success = $('#bookmark_success-alert');
   // notice(通知系)アラート
   setTimeout(function(){
-    notice.slideUp(500);
+    notice.slideUp(400);
   },4500);
-  notice.slideDown(200);
+  notice.slideDown(400);
   // 非ログイン時の(エラー)アラート
   $('.like_button,.bookmark_button').on('click', function() {
     var error = $(this).attr('class');
--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,17 +1,47 @@
-// devise関連フォーム入力のエフェクト
-// ユーザ登録情報編集フォーム用にfocus追加
 $(function() {
-  var form = $(".form-x");
+  var passWordInput = $('#user_password'),
+      passWordCheckButton = $('.password_check_button');
 
-  form.on('input focus', function(){
-    var formId = $(this).attr('id');
-    var input = $(this).val().length;
-    if (input === 0) {
-      $(`#${formId}`).removeClass('form-animation');
-      $(`#${formId}-label`).removeClass('label-animation');
+  $(function() {
+    var form = $(".form-x");
+    // devise関連フォーム入力のエフェクト
+  // ユーザ登録情報編集フォーム用にfocus追加
+    form.on('input focus', function(){
+      var formId = $(this).attr('id');
+      var input = $(this).val().length;
+      if (input === 0) {
+        $(`#${formId}`).removeClass('form-animation');
+        $(`#${formId}-label`).removeClass('label-animation');
+      } else {
+        $(`#${formId}`).addClass('form-animation');
+        $(`#${formId}-label`).addClass('label-animation');
+      }
+    });
+  });
+
+  $(function() {
+    // パスワード表示/非表示ボタンの表示
+    passWordInput.on('input focus', function(){
+      var input = $(this).val().length;
+      if (input === 0) {
+        passWordCheckButton.addClass('none');
+      } else {
+        passWordCheckButton.removeClass('none');
+      }
+    });
+  });
+
+  $(function() {
+    // パスワードの中身を表示/非表示
+    passWordCheckButton.on('click', function(){
+      var type = passWordInput.attr('type');
+    if (type === 'password') {
+      passWordCheckButton.text('非表示にする');
+      passWordInput.attr('type','text');
     } else {
-      $(`#${formId}`).addClass('form-animation');
-      $(`#${formId}-label`).addClass('label-animation');
+      passWordCheckButton.text('表示する');
+      passWordInput.attr('type','password');
     }
+    });
   });
 });
