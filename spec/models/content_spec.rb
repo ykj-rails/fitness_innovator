4919eb0728d921fd619ae1adaf5deb8bdbcd389a
Yuki M <yukiji.0307@gmail.com>
2019-04-10 15:56:54 +0900

    Contentモデル単体テスト完了

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -7,11 +7,23 @@ describe Content do
       expect(content).to be_valid
     end
 
-    # it "textが空だと登録できない" do
-    #   comment = build(:comment, text: "")
-    #   comment.valid?
-    #   expect(comment.errors[:text]).to include("を入力してください。")
-    # end
+    it "before_body_weightが空だと登録できない" do
+      content = build(:content, before_body_weight: "")
+      content.valid?
+      expect(content.errors[:before_body_weight]).to include("を入力してください。")
+    end
+
+    it "after_body_weightが空だと登録できない" do
+      content = build(:content, after_body_weight: "")
+      content.valid?
+      expect(content.errors[:after_body_weight]).to include("を入力してください。")
+    end
+
+    it "periodが空だと登録できない" do
+      content = build(:content, period: "")
+      content.valid?
+      expect(content.errors[:period]).to include("を入力してください。")
+    end
 
   end
 end
