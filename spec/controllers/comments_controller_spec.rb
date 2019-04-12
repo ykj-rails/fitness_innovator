1a3afcbd96f811ccab9a92af920473c38ca3e489
Yuki M <yukiji.0307@gmail.com>
2019-04-12 15:04:36 +0900

    Comments_controller単体テスト完了

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,23 +1,18 @@
 require 'rails_helper'
 
-describe LikesController do
+describe CommentsController do
   let(:user) { create(:user) }
   let(:content) { create(:content) }
   describe 'POST #create' do
-    let(:params) { { user_id: user.id, content_id: content.id, like: attributes_for(:like) } }
+    let(:params) { { user_id: user.id, content_id: content.id, comment: attributes_for(:comment) } }
     subject { post :create, params: params }
     context 'ログインしている場合' do
       before do
         login user
       end
 
-      it "アクション内で定義されている@contentの値が期待したものになるか" do
-        subject
-        expect(assigns(:content)).to eq content
-      end
-
-      it 'Likeモデルのレコードが1件追加される' do
-        expect{ subject }.to change(Like, :count).by(1)
+      it 'Commentモデルのレコードが1件追加される' do
+        expect{ subject }.to change(Comment, :count).by(1)
       end
 
       it 'root_pathに遷移' do
@@ -27,7 +22,7 @@ describe LikesController do
     end
 
     context 'ログインしていない場合' do
-      it "sessions/new.html.erbに遷移" do
+      it "root_pathに遷移" do
         subject
         expect(response).to redirect_to(new_user_session_path)
       end
@@ -35,22 +30,22 @@ describe LikesController do
   end
 
   describe 'DELETE #destroy' do
-    let(:like) { like = create(:like, content_id: content.id, user_id: user.id) }
-    let(:params) { { user_id: user.id, id: like, like: attributes_for(:like) } }
+    let(:comment) { comment = create(:comment, content_id: content.id, user_id: user.id) }
+    let(:params) { { user_id: user.id, content_id: content.id, id: comment, comment: attributes_for(:comment) } }
     subject { delete :destroy, params: params }
     context 'ログインしている場合' do
       before do
         login user
       end
 
-      it "アクション内で定義されている@contentの値が期待したものになるか" do
+      it "アクション内で定義されている@commentの値が期待したものになるか" do
         subject
-        expect(assigns(:content)).to eq content
+        expect(assigns(:comment)).to eq comment
       end
 
-      it 'Bookmarkモデルのレコードが1件削除される' do
-        like.reload
-        expect{ subject }.to change(Like, :count).by(-1)
+      it 'Commentモデルのレコードが1件削除される' do
+        comment.reload
+        expect{ subject }.to change(Comment, :count).by(-1)
       end
 
       it 'root_pathに遷移' do
