999669df545fb1ea8c4d21daf5fd4b740815f50a
Yuki M <yukiji.0307@gmail.com>
2019-04-15 21:11:05 +0900

    本番環境用メーラー設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # default url
+  config.action_mailer.default_url_options = { host: 'http://52.194.24.80:3000/' }
+  # mail setting
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "fitnessinnovator.jp@gmail.com",
+    :password => ENV['GMAIL_PASS'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
