ae2c7060d51a1718f26ce79aef7b333d40757452
Yuki M <yukiji.0307@gmail.com>
2019-04-20 19:53:03 +0900

    EXIF再調整

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -10,18 +10,18 @@ module ContentsHelper
   end
 
   def resize_s(img)
-    return img.variant(combine_options: { resize:"150x150^", crop:"150x150+0+0", gravity: :center, strip: true }).processed
+    return img.variant(combine_options: { resize:"150x150^", crop:"150x150+0+0", gravity: :center, auto_orient: true }).processed
   end
 
   def resize_sm(img)
-    return img.variant(combine_options: { resize:"300x300^", crop:"300x300+0+0", gravity: :center, strip: true }).processed
+    return img.variant(combine_options: { resize:"300x300^", crop:"300x300+0+0", gravity: :center, auto_orient: true }).processed
   end
 
   def resize_m(img)
-    return img.variant(combine_options: { resize:"480x480^", crop:"480x480+0+0", gravity: :center, strip: true }).processed
+    return img.variant(combine_options: { resize:"480x480^", crop:"480x480+0+0", gravity: :center, auto_orient: true }).processed
   end
 
   def resize_l(img)
-    return img.variant(combine_options: { resize:"460x680^", crop:"680x680+0+0", gravity: :center, strip: true }).processed
+    return img.variant(combine_options: { resize:"460x680^", crop:"680x680+0+0", gravity: :center, auto_orient: true }).processed
   end
 end
