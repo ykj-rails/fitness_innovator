e7fdd89c7ad547f4d21f04305af1cde57844a2af
Yuki M <yukiji.0307@gmail.com>
2019-04-27 20:46:16 +0900

    contet/indexのコメント表示を3県に変更

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,10 +1,10 @@
 module CommentsHelper
   # 最新のコメント4件の取得
-  def get_4_comments(comments)
-    return comments = comments[-4..-1]
+  def get_3_comments(comments)
+    return comments = comments[-3..-1]
   end
   # 最新のコメント4件を除いたコメントの取得
   def get_remaining_comments(comments)
-    return comments = comments[0..-5]
+    return comments = comments[0..-4]
   end
 end
