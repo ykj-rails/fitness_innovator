f6fb28309a14f409080069cf4aea8ca3a948b25d
Yuki M <yukiji.0307@gmail.com>
2019-04-27 16:27:55 +0900

    cdn修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require activestorage
 //= require jquery
 //= require rails-ujs
+//= require activestorage
 //= require cocoon
 //= require_tree .
 //= require popper
